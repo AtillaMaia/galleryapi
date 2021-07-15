58852f9040f9b4c71142332c758930fbed095398
anmaia <anmaia@sfiec.org.br>
2021-07-15 17:53:48 -0300

    deploy heroku

--- gallery/settings.py
+++ gallery/settings.py
@@ -1,5 +1,6 @@
 import os
 import environ
+import dj_database_url
 
 from datetime import timedelta
 from pathlib import Path
@@ -92,10 +93,7 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(default=f"sqlite:///{BASE_DIR}/db.sqlite3")
 }
 
 
